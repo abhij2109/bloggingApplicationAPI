a48751a74ab9b0d96e8043c9fe25dd7eb23c9108
Abhi <abhij2116@gmail.com>
2022-04-15 23:40:27 +0530

    User Repository layer completed.

--- src/main/java/com/abhi/blogapplication/repositories/UserRepository.java
+++ src/main/java/com/abhi/blogapplication/repositories/UserRepository.java
@@ -4,4 +4,5 @@ import com.abhi.blogapplication.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
 }
