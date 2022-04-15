6ef4e72a0e215785b977583b24fdb2b60f197e66
Abhi <abhij2116@gmail.com>
2022-04-15 23:40:05 +0530

    Category Repository layer completed.

--- src/main/java/com/abhi/blogapplication/repositories/CategoryRepository.java
+++ src/main/java/com/abhi/blogapplication/repositories/CategoryRepository.java
@@ -4,4 +4,5 @@ import com.abhi.blogapplication.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category,Integer> {
+
 }
